844f49573d8023e131c97667af038f61b8c98fe4
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-09-24 00:21:52 +0530

    Add router

--- helpers/routes.js
+++ helpers/routes.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const constants = require('./constants');
+
+router.get('/', (req, res)=>{
+    res.json({"message": "Here are the available resources",
+        "resources": ["alias"]})
+});
 
 
 module.exports = router;
