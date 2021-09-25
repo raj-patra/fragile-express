49ad6968750ac33f973bfa6d8f630a680c83c278
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-09-25 15:43:59 +0530

    Updated callback function

--- helpers/routes.js
+++ helpers/routes.js
@@ -2,16 +2,32 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 const constants = require('./constants');
-const co = require('co');
-const { response } = require("express");
 
 
-router.get('/', (req, res, next)=>{
+function fetch_response(url, res){
+    (async () => {
+        try{
+            await axios.get(url)
+                        .then(data => {
+                            res.status(200).send({"message": "Data fetch successful.", 
+                                                    "data": data.data, 
+                                                    "reference_api": data.config.url})
+                        })
+                        .catch(error => res.send(error))
+                        .next;
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }) ();
+}
+
+router.get('/', (req, res)=>{
     res.status(200).send({"message": "List of available resources.",
         "resources": ["alias"]})
 });
 
-router.get('/alias', (req, res, next)=>{
+router.get('/alias', (req, res)=>{
     function randomChoice(arr) {
         return arr[Math.floor(arr.length * Math.random())];
     }
@@ -19,39 +35,10 @@ router.get('/alias', (req, res, next)=>{
         "data": randomChoice(constants.adjective)+'-'+randomChoice(constants.noun)})
 });
 
-router.get('/insult', (req, res)=>{
-    (async () => {
-        try{
-            await axios.get(constants.api_urls.insult)
-                        .then(data => {
-                            res.status(200).send({"message": "Data fetch successful.", 
-                                                    "data": data.data, 
-                                                    "reference_api": data.config.url})
-                                                })
-                        .catch(error => res.send(error));
-        }
-        catch (error) {
-            console.log(error)
-        }
-    }) ();
-});
+router.get('/insult', (req, res)=> fetch_response(constants.api_urls.insult, res));
 
-router.get('/fact', (req, res)=>{
-    (async () => {
-        try{
-            await axios.get(constants.api_urls.fact)
-                        .then(data => {
-                            res.status(200).send({"message": "Data fetch successful.", 
-                                                    "data": data.data, 
-                                                    "reference_api": data.config.url})
-                                                })
-                        .catch(error => res.send(error));
-        }
-        catch (error) {
-            console.log(error)
-        }
-    }) ();
-});
+router.get('/fact', (req, res)=> fetch_response(constants.api_urls.fact, res));
+
 
 
 module.exports = router;
