2887ab4a3c3dd466db01bf6845000e31c654fd49
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-09-26 13:47:08 +0530

    Minor change

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const api = require('./helpers/routes');
 const constants = require('./helpers/constants');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "localhost"
+const PORT = process.env.PORT || 80;
+const HOST = process.env.HOST || "http://localhost/"
 
 app.use([express.json(), express.urlencoded({extended: true})]);
 app.use('/api', api);
@@ -17,4 +17,4 @@ app.get('/about', (req, res)=>{
     res.status(200).send(constants.about)
 });
 
-app.listen(PORT, () => {console.log("Server is listening at http://%s:%s/", HOST, PORT);});
+app.listen(PORT, () => {console.log("Server is listening at %s", HOST);});
