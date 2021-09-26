81c6d7c87043b83a5a1190c3888167dc6058bb5e
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-09-26 12:59:52 +0530

    Added Procfile

--- helpers/routes.js
+++ helpers/routes.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const constants = require('./constants');
 const quote = require('./quotes');
 
-
 function fetch_response(url, res){
     (async () => {
         try{
