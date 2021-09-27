c95f4c910b7d8d1c1c88f30d78eb5ca0657edd4a
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-09-27 10:53:43 +0530

    Added /quote/superhero

--- helpers/quotes.js
+++ helpers/quotes.js
@@ -30,5 +30,6 @@ router.get('/', (req, res)=>{
 router.get('/random', (req, res)=> fetch_response(constants.api_urls.quote, res));
 router.get('/kanye', (req, res)=> fetch_response(constants.api_urls.kanye, res));
 router.get('/trump', (req, res)=> fetch_response(constants.api_urls.trump, res));
+router.get('/superhero', (req, res)=> fetch_response(constants.api_urls.superhero, res));
 
 module.exports = router;
