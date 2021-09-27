f79b89e7ea05de4260a1bcd25881f878d77ba778
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-09-27 10:55:31 +0530

    Added resources for /quote

--- helpers/quotes.js
+++ helpers/quotes.js
@@ -24,7 +24,7 @@ function fetch_response(url, res){
 
 router.get('/', (req, res)=>{
     res.status(200).send({"message": "List of available resources.",
-        "resources": ["random", "kanye", "trump"]})
+        "resources": ["random", "kanye", "trump", "superhero"]})
 });
 
 router.get('/random', (req, res)=> fetch_response(constants.api_urls.quote, res));
