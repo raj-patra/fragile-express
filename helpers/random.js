0091627b8d018638cbd26e810487c8b2c735d138
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-10-13 11:26:07 +0530

    Added random.js

---
+++ helpers/random.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+
+const constants = require('./constants');
+
+function fetch_response(url, res){
+    (async () => {
+        try{
+            await axios.get(url)
+                        .then(data => {
+                            res.status(200).send({"message": "Data fetch successful.", 
+                                                    "data": data.data, 
+                                                    "reference_api": data.config.url,
+                                                    "root": constants.host})
+                        })
+                        .catch(error => res.send(error))
+                        .next;
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }) ();
+}
+
+router.get('/', (req, res)=>{
+    res.status(200).send(constants.random)
+});
+
+
+router.get('/alias', (req, res)=>{
+    function randomChoice(arr) {
+        return arr[Math.floor(arr.length * Math.random())];
+    }
+    res.status(200).send({"message": "Random alias generated.",
+        "data": randomChoice(constants.adjective)+'-'+randomChoice(constants.noun),
+        "root": constants.host})
+});
+
+router.get('/website', (req, res)=> {
+    
+    axios.get(constants.api_urls.random.website)
+        .then(response => {
+            var data = response.data.split(`iframe src="`)[1].split(`">`)[0].split(`" title="`)
+            res.redirect(data[0]);
+        })
+        .catch(error => res.send(error));
+});
+
+module.exports = router;
