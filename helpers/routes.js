6f26c7441c68d5d05f85468863d2ac5cb56fe10f
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-10-13 11:25:10 +0530

    Added /alias & /website inside /random/

--- helpers/routes.js
+++ helpers/routes.js
@@ -8,6 +8,7 @@ const quotes = require('./quotes');
 const facts = require('./facts');
 const personalities = require('./personalities');
 const games = require('./games');
+const random = require('./random');
 
 function fetch_response(url, res){
     (async () => {
@@ -32,28 +33,12 @@ router.get('/', (req, res)=>{
     res.status(200).send(constants.api)
 });
 
-router.get('/alias', (req, res)=>{
-    function randomChoice(arr) {
-        return arr[Math.floor(arr.length * Math.random())];
-    }
-    res.status(200).send({"message": "Random alias generated.",
-        "data": randomChoice(constants.adjective)+'-'+randomChoice(constants.noun)})
-});
-
-router.get('/bored', (req, res)=> {
-    
-    axios.get(constants.api_urls.random_website)
-        .then(response => {
-            var data = response.data.split(`iframe src="`)[1].split(`">`)[0].split(`" title="`)
-            res.redirect(data[0]);
-        })
-        .catch(error => res.send(error));
-});
 
 router.use("/quotes", quotes);
 router.use("/facts", facts);
 router.use("/personalities", personalities);
 router.use("/games", games);
+router.use("/random", random);
 
 
 module.exports = router;
