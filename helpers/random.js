b379e974ce13ee926e379aae77ee43a7f08279e9
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-12-17 10:53:03 +0530

    Added activity in random

--- helpers/random.js
+++ helpers/random.js
@@ -4,10 +4,39 @@ const router = express.Router();
 
 const constants = require('./constants');
 
+function fetch_response(url, res){
+    const headers = {"Accept": "application/json"};
+    (async () => {
+        try{
+            await axios.get(url, {
+                headers: headers
+                })
+                    .then(data => {
+                        if (data.status == 200){
+                            res.status(200).send({"message": "Data fetch successful.", 
+                                                    "data": data.data, 
+                                                    "reference_api": data.config.url,
+                                                    "root": constants.host})
+                        }
+                        else{
+                            res.status(data.status).send({"message": "Data fetch unsuccessful.", 
+                                                    "data": null, 
+                                                    "reference_api": data.config.url,
+                                                    "root": constants.host})
+                        }
+                    })
+                    .catch(error => res.send(error))
+                    .next;
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }) ();
+}
+
 router.get('/', (req, res)=>{
     res.status(200).send(constants.random)
 });
-
 
 router.get('/alias', (req, res)=>{
     function randomChoice(arr) {
@@ -30,5 +59,7 @@ router.get('/website', (req, res)=> {
 router.get('/human', (req, res)=> {
     res.redirect(constants.api_urls.random.human);
 });
+
+router.get('/activity', (req, res)=> fetch_response(constants.api_urls.random.activity, res));
 
 module.exports = router;
