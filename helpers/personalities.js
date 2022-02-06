f5cd090bb08a17403bd50c1d04dee593133d8eef
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-02-06 12:40:57 +0530

    Refactoring API - personalities

--- helpers/personalities.js
+++ helpers/personalities.js
@@ -4,25 +4,6 @@ const router = express.Router();
 
 const constants = require('./constants');
 
-function fetch_response(url, res){
-    (async () => {
-        try{
-            await axios.get(url)
-                        .then(data => {
-                            res.status(200).send({"message": "Data fetch successful.", 
-                                                    "data": data.data, 
-                                                    "reference_api": data.config.url,
-                                                    "root": constants.host})
-                        })
-                        .catch(error => res.send(error))
-                        .next;
-        }
-        catch (error) {
-            console.log(error)
-        }
-    }) ();
-}
-
 router.get('/', (req, res)=>{
     res.status(200).json(constants.personalities)
 });
