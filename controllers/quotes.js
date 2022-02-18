51fc91741c63ab9cc7983dc39621ae0a46aabc15
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-02-18 11:42:41 +0530

    Restructured project - quotes

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -3,53 +3,54 @@ const axios = require("axios");
 const router = express.Router();
 
 const constants = require('../helpers/constants');
+const utils = require('../helpers/utils');
 
 router.get('/', (req, res)=>{
     res.status(200).json(constants.quotes)
 });
 
 router.get('/random', async(req, res)=>{
-    let data = await constants.fetch_response(constants.api_urls.quotes.random);
+    let data = await utils.fetch_response(constants.api_urls.quotes.random);
     res.status(200).json(data);
 });
 
 router.get('/kanye', async(req, res)=>{
-    let data = await constants.fetch_response(constants.api_urls.quotes.kanye);
+    let data = await utils.fetch_response(constants.api_urls.quotes.kanye);
     res.status(200).json(data);
 });
 
 router.get('/trump', async(req, res)=>{
-    let data = await constants.fetch_response(constants.api_urls.quotes.trump);
+    let data = await utils.fetch_response(constants.api_urls.quotes.trump);
     res.status(200).json(data);
 });
 
 router.get('/superhero', async(req, res)=>{
-    let data = await constants.fetch_response(constants.api_urls.quotes.superhero);
+    let data = await utils.fetch_response(constants.api_urls.quotes.superhero);
     res.status(200).json(data);
 });
 
 router.get('/poems', async(req, res)=>{
-    let data = await constants.fetch_response(constants.api_urls.quotes.poems);
+    let data = await utils.fetch_response(constants.api_urls.quotes.poems);
     res.status(200).json(data);
 });
 
 router.get('/anime', async(req, res)=>{
-    let data = await constants.fetch_response(constants.api_urls.quotes.anime);
+    let data = await utils.fetch_response(constants.api_urls.quotes.anime);
     res.status(200).json(data);
 });
 
 router.get('/powerful', async(req, res)=>{
-    let data = await constants.fetch_response(constants.api_urls.quotes.powerful);
+    let data = await utils.fetch_response(constants.api_urls.quotes.powerful);
     res.status(200).json(data);
 });
 
 router.get('/stoicism', async(req, res)=>{
-    let data = await constants.fetch_response(constants.api_urls.quotes.stoicism);
+    let data = await utils.fetch_response(constants.api_urls.quotes.stoicism);
     res.status(200).json(data);
 });
 
 router.get('/got', async(req, res)=>{
-    let data = await constants.fetch_response(constants.api_urls.quotes.game_of_thrones);
+    let data = await utils.fetch_response(constants.api_urls.quotes.game_of_thrones);
     res.status(200).json(data);
 });
 
