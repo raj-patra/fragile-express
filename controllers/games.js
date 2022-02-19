33b4302886fb26298b81e8fb1b2121a1a1cc68fb
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-02-19 15:23:21 +0530

    Added utils.random_choice to games/browser

--- controllers/games.js
+++ controllers/games.js
@@ -21,7 +21,7 @@ router.get('/pc', async(req, res)=>{
 
 router.get('/browser', async(req, res)=>{
     let data = await utils.fetch_response(constants.api_urls.games.browser);
-    data.data = randomChoice(data.data);
+    data.data = utils.random_choice(data.data);
     res.status(200).json(data);
 });
 
