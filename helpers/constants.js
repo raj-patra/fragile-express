c646ab7ce56c3797bf1cb9c51a3f69d373120121
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-02-26 11:42:48 +0530

    [chore]: Added quiz arguments to constants

--- helpers/constants.js
+++ helpers/constants.js
@@ -123,4 +123,35 @@ module.exports = {
         'thoughtful', 'thoughtless', 'tired', 'smoggy', 'sore', 'sparkling', 'splendid', 'spotless', 'stormy', 'strange', 'stupid', 'successful', 'super ', 'svelte', 'wild', 'witty', 'worried', 'worrisome', 'wrong', 'zany', 'zealous', 'tough', 'troubled', 'ugliest', 'ugly', 'uninterested', 'unsightly', 'unusual', 'upset',
         'uptight', 'useful'
     ],
+
+    quiz_arguments: {
+        difficulty: ["easy", "medium", "hard"],
+        category:   {
+            "General Knowledge": 9,
+            "Entertainment: Books": 10,
+            "Entertainment: Film": 11,
+            "Entertainment: Music": 12,
+            "Entertainment: Musicals & Theatres": 13,
+            "Entertainment: Television": 14,
+            "Entertainment: Video Games": 15,
+            "Entertainment: Board Games": 16,
+            "Science & Nature": 17,
+            "Science: Computers": 18,
+            "Science: Mathematics": 19,
+            "Mythology": 20,
+            "Sports": 21,
+            "Geography": 22,
+            "History": 23,
+            "Politics": 24,
+            "Art": 25,
+            "Celebrities": 26,
+            "Animals": 27,
+            "Vehicles": 28,
+            "Entertainment: Comics": 29,
+            "Science: Gadgets": 30,
+            "Entertainment: Japanese Anime & Manga": 31,
+            "Entertainment: Cartoon & Animations": 32,
+        },
+        type: ["multiple", "boolean"],
+    }
 }
