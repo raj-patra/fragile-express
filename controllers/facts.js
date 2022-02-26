4884486fa4720e7f7eb8063dc3280b75de191284
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-02-26 11:30:46 +0530

    [feature]: Made /facts/quiz dynamic

--- controllers/facts.js
+++ controllers/facts.js
@@ -51,7 +51,7 @@ router.get('/cat', async(req, res)=>{
 
 router.get('/quiz', async(req, res)=>{
     let query_string = "&"+req.url.split("?")[1]
-    let data = await utils.fetch_response(constants.api_urls.facts.quiz);
+    let data = await utils.fetch_response(constants.api_urls.facts.quiz+query_string);
     res.status(200).json(data);
 });
 
