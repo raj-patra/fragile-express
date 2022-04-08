3cf52b42be99380ff461053f6299c30be7cd91b4
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-04-08 12:25:17 +0530

    [chore]: Removed redundant endpoints in /jokes

--- controllers/jokes.js
+++ controllers/jokes.js
@@ -21,24 +21,4 @@ router.get('/:option', async(req, res)=>{
     }
 });
 
-// router.get('/random', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.jokes.random);
-//     res.status(200).json(data);
-// });
-
-// router.get('/chuck_norris', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.jokes.chuck_norris);
-//     res.status(200).json(data);
-// });
-
-// router.get('/dad', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.jokes.dad);
-//     res.status(200).json(data);
-// });
-
-// router.get('/yo_mama', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.jokes.yo_mama);
-//     res.status(200).json(data);
-// });
-
 module.exports = router;
