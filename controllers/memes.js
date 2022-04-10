474499e501e55bbe6e8e8784a0cc64e968b9cbce
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-04-10 22:12:43 +0530

    [chore]: Removed redundant req parameter

--- controllers/memes.js
+++ controllers/memes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const constants = require('../helpers/constants');
 const utils = require('../helpers/utils');
 
-router.get('/', (req, res)=>{
+router.get('/', (_, res)=>{
     res.status(200).json(constants.memes);
 });
 
