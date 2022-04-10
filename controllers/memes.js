a2d2d0f678be9e605cc014b6fc0e4ca9a77864f5
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-04-10 22:11:03 +0530

    [chore]: Removed redundant endpoints in /memes

--- controllers/memes.js
+++ controllers/memes.js
@@ -20,34 +20,4 @@ router.get('/:option', async(req, res)=>{
     }
 });
 
-// router.get('/random', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.memes.random);
-//     res.status(200).json(data);
-// });
-
-// router.get('/reddit',async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.memes.reddit);
-//     res.status(200).json(data);
-// });
-
-// router.get('/namo', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.memes.namo);
-//     res.status(200).json(data);
-// });
-
-// router.get('/templates', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.memes.templates);
-//     res.status(200).json(data);
-// });
-
-// router.get('/yes', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.memes.yes);
-//     res.status(200).json(data);
-// });
- 
-// router.get('/no', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.memes.no);
-//     res.status(200).json(data);
-// });
-
 module.exports = router;
