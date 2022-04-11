9b639a7c9287f82112f42a4ecb626c447927636d
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-04-11 10:35:36 +0530

    [chore]: Removed redundant req parameter

--- controllers/personalities.js
+++ controllers/personalities.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const constants = require('../helpers/constants');
 const utils = require('../helpers/utils');
 
-router.get('/', (req, res)=>{
+router.get('/', (_, res)=>{
     res.status(200).json(constants.personalities);
 });
 
@@ -22,46 +22,5 @@ router.get('/:option', async(req, res)=>{
         });
     }
 });
-
-// router.get('/insult', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.personalities.insult);
-//     res.status(200).json(data);
-// });
-
-// router.get('/advice', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.personalities.advice);
-//     res.status(200).json(data);
-// });
-
-// router.get('/affirmation', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.personalities.affirmation);
-//     res.status(200).json(data);
-// });
-
-// router.get('/inspiration', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.personalities.inspiration);
-//     data.data = utils.random_choice(data.data);
-//     res.status(200).json(data);
-// });
-
-// router.get('/kanye', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.personalities.kanye);
-//     res.status(200).json(data);
-// });
-
-// router.get('/trump', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.personalities.trump);
-//     res.status(200).json(data);
-// });
-
-// router.get('/chuck_norris', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.personalities.chuck_norris);
-//     res.status(200).json(data);
-// });
-
-// router.get('/ron_swanson', async(req, res)=>{
-//     let data = await utils.fetch_response(constants.api_urls.personalities.ron_swanson);
-//     res.status(200).json(data);
-// });
 
 module.exports = router;
