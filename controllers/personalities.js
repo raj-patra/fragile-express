c623469698ab88f7cfd0cb3b8f2c31f40ffe3b92
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-04-11 10:35:10 +0530

    [chore]: Removed redundant endpoints in /personalities

--- controllers/personalities.js
+++ controllers/personalities.js
@@ -12,6 +12,9 @@ router.get('/:option', async(req, res)=>{
     let option = req.params.option;
     if (option in constants.api_urls.personalities){
         let data = await utils.fetch_response(constants.api_urls.personalities[option]);
+        if (option === 'inspiration'){
+            data.data = utils.random_choice(data.data);
+        }
         res.status(200).json(data);
     } else {
         res.status(404).json({
