9849d0afa45a4aafc994f023eeaa4a93899a3240
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-04-13 22:57:30 +0530

    [chore]: Modularized /quotes

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -4,8 +4,23 @@ const router = express.Router();
 const constants = require('../helpers/constants');
 const utils = require('../helpers/utils');
 
-router.get('/', (req, res)=>{
+router.get('/', (_, res)=>{
     res.status(200).json(constants.quotes);
+});
+
+router.get('/:option', async(req, res)=>{
+    let option = req.params.option;
+    if (option in constants.api_urls.quotes){
+        let data = await utils.fetch_response(constants.api_urls.quotes[option]);
+        if (option === 'inspiration'){
+            data.data = utils.random_choice(data.data);
+        }
+        res.status(200).json(data);
+    } else {
+        res.status(404).json({
+            error: 'Endpoint does not exist'
+        });
+    }
 });
 
 router.get('/random', async(req, res)=>{
