007c968f8bc2cb19aa765293933f96f7cf5f125c
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-07-22 23:44:46 +0530

    [refactor]: Removed trailing spaces

--- controllers/games.js
+++ controllers/games.js
@@ -25,10 +25,10 @@ router.get(['/valorant', '/valorant/:option'], async(req, res)=>{
     let url = (option) ? constants.api_urls.games.valorant+option : constants.api_urls.games.valorant+"agents";
     console.log(url)
     let data = await utils.fetch_response(url);
-    
+
     data.data = utils.random_choice(data.data.data);
     data.options = constants.valo_options;
-    
+
     res.status(200).json(data);
 });
 
--- controllers/shows.js
+++ controllers/shows.js
@@ -12,10 +12,10 @@ router.get(['/harry_potter', '/harry_potter/:option'], async(req, res)=>{
     let option = req.params.option;
     let url = (option) ? constants.api_urls.shows.harry_potter+option : constants.api_urls.shows.harry_potter;
     let data = await utils.fetch_response(url);
-    
+
     data.data = utils.random_choice(data.data);
     data.options = constants.hp_options;
-    
+
     res.status(200).json(data);
 });
 
