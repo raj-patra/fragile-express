6817bb68eb38544086ae295466aac916a17f78c4
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-07-23 23:45:55 +0530

    [refactor]: Standardized usage of quotes

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require("express");
-const api = require('./controllers/routes');
-const constants = require('./helpers/constants');
+const api = require("./controllers/routes");
+const constants = require("./helpers/constants");
 const app = express();
 
 const PORT = process.env.PORT || 80;
 const HOST = process.env.HOST || "http://localhost"
 
 app.use([express.json(), express.urlencoded({extended: true})]);
-app.use('/api', api);
+app.use("/api", api);
 
-app.get('/', (_, res)=>{
+app.get("/", (_, res)=>{
     res.status(200).send(constants.root);
 });
 
-app.get('/about', (_, res)=>{
+app.get("/about", (_, res)=>{
     res.status(200).send(constants.about);
 });
 
-app.get('/reference', (_, res)=>{
+app.get("/reference", (_, res)=>{
     res.status(200).send(constants.api_urls);
 });
 
-app.get('*', (_, res)=>{
+app.get("*", (_, res)=>{
     res.status(404).send(constants[404]);
 });
 
